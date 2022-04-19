880e1826b91a684914e69d743e3855e606e180b2
ml <miklakt@gmail.com>
2022-04-20 02:53:18 +0300

    ignore fields

--- sfbox_utils/read_output.py
+++ sfbox_utils/read_output.py
@@ -4,6 +4,7 @@ from typing import Union
 from enum import Enum, auto
 
 
+
 from .utils import try_cast_to_numeric, ld_to_dl
 
 
@@ -13,7 +14,7 @@ class ParseError(ValueError):
 
 class OutputLineType(Enum):
     empty = auto()
-    comment = auto()
+    ignore = auto()
     block_separator = auto()
     statement = auto()
     invalid = auto()
@@ -123,7 +124,27 @@ def parse_file(
         convert_value_to = try_cast_to_numeric,
         compact = True,
         to_dict = True,
+        ignore_fields = None, 
+        read_fields = None,
         **kwargs):
+    if (ignore_fields is not None) and (read_fields is not None):
+        raise AttributeError("Can not pass ignore_fields and read_fields at the same time")
+    
+    def field_to_skip(line_, linetype_):
+        if linetype_ is OutputLineType.vector_element: return False
+        
+        if linetype is OutputLineType.statement:
+            line_ = line_.rsplit(":",1)[0]
+        line_ = line_.rstrip()
+        if (ignore_fields is None) and (read_fields is None):
+            return False
+        elif (ignore_fields is not None):
+            return line_ in ignore_fields
+        else:
+            return line_ not in read_fields
+
+    __SKIP__ = True
+
     f = open(file)
     #to collect all calculations in a list
     blocks = []
@@ -135,11 +156,13 @@ def parse_file(
     vector_acc = []
     #reading file line by line
     i=0
+    #skip vector flag
+    skip_vector = False
     while line := f.readline():
         line = line.rstrip('\r\n')
         i=i+1
         linetype = get_line_type(line)
-        #print(i,line, linetype)
+        skip_field = field_to_skip(line, linetype)
 
         if linetype == OutputLineType.invalid:
             raise ParseError(f"Invalid expression in line {i}")
@@ -149,22 +172,32 @@ def parse_file(
             #When we meet vector element, we must have read vector name
             if vector_name is None:
                 raise ParseError(f"Vector element is read but no vector name found, line {i}")
-            vector_acc.append(line)
+            
+            if not skip_vector:
+                vector_acc.append(line)
+            else:
+                #we do not need to read vector, 
+                #but we indicate we indeed read vector elements
+                vector_acc = __SKIP__
 
         else:
             if vector_acc:
-                lines.append(parse_vector(vector_name, vector_acc, convert_to=convert_vector_to, to_dict=to_dict, **kwargs))
+                if not skip_vector:
+                    lines.append(parse_vector(vector_name, vector_acc, convert_to=convert_vector_to, to_dict=to_dict, **kwargs))
                 vector_name = None
                 vector_acc = []
+            
             else:
                 if vector_name is not None:
                     raise ParseError(f"Vector name must be followed by vector elements, line {i}")
 
         if linetype == OutputLineType.vector_name:
             vector_name = line
+            skip_vector = skip_field
 
         if linetype == OutputLineType.statement:
-            lines.append(parse_statement(line, convert_to=convert_value_to, to_dict=to_dict, **kwargs))
+            if not skip_field:
+                lines.append(parse_statement(line, convert_to=convert_value_to, to_dict=to_dict, **kwargs))
 
         if linetype == OutputLineType.block_separator:
             if to_dict:
