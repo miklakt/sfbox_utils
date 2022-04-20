3886f00fa45407b6e7253d26e5a8c36fd1d9d7cb
ml <miklakt@gmail.com>
2022-04-21 02:39:02 +0300

    minor

--- sfbox_utils/scripts/split_output.sh
+++ sfbox_utils/scripts/split_output.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mkdir "${1%.*}"
 cd "${1%.*}"
-csplit "../$1" -n3 '/system delimiter/' '{*}' --suppress-matched --prefix "${1%.*}" --suffix-format "%03d.out"
+csplit "../$1" -n3 '/system delimiter/' '{*}' --suppress-matched --prefix "${1%.*}" --suffix-format "_%03d.out"
