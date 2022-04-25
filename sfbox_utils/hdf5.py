4343c915f54224981a60a23eb8ceac7cb029f92f
ml <miklakt@gmail.com>
2022-04-26 02:14:59 +0300

    pass routine to proccess data befor storing to hdf5

--- sfbox_utils/hdf5.py
+++ sfbox_utils/hdf5.py
@@ -1,13 +1,21 @@
 import pathlib
-from typing import Union, Callable, List
+from typing import Any, Dict, Union, Callable, List, Optional
 import h5py
 import numpy as np
 import logging
+
 logger = logging.getLogger(__name__)
 
 from .read_output import parse_file
 
-def store_calculation_result(hdf_group : h5py.Group, data : dict, process_routine : Callable = None):
+RoutineArgType = Optional[Callable[[Dict[str, Any]], Dict[str, Any]]]
+FieldsArg = Optional[List]
+
+def store_calculation_result(
+    hdf_group : h5py.Group, 
+    data : dict, 
+    process_routine : RoutineArgType = None
+        ):
     
     if process_routine is not None:
         logger.debug("routine called")
@@ -19,37 +27,45 @@ def store_calculation_result(hdf_group : h5py.Group, data : dict, process_routin
         else:
             hdf_group.attrs.create(k, v)
 
-def store_file(hdf_group : h5py.Group, groupname : str, reader, process_routine):
+def store_file(
+    hdf_group : h5py.Group, 
+    groupname : str, 
+    reader, 
+    process_routine : RoutineArgType = None,
+        ):
+    
     i=0
     new_groupname = groupname
     #resolve conflict if the same group already exists
     while True:
         if new_groupname in hdf_group: 
-            new_groupname = groupname + "_{i}"
+            new_groupname = groupname + f"_{i}"
             i = i+1
         else:
             break
     if new_groupname != groupname:
         logger.warning("Group name already exist, new name assigned")
     
-    g = hdf_group.create_group(new_groupname, process_routine)
+    g = hdf_group.create_group(new_groupname)
 
     for i, data in enumerate(reader):
         gg = g.create_group(str(i))
-        store_calculation_result(gg, data)
+        store_calculation_result(gg, data, process_routine)
 
     logger.info("File is stored in hdf5 database")
 
 def store_files(
             dir : Union[str,pathlib.Path], 
             hdf_group : h5py.Group, 
-            ignore_fields = None, 
-            read_fields = None,
-            read_fields_regex = None,
-            process_routine = None,
+            ignore_fields : FieldsArg = None, 
+            read_fields : FieldsArg = None,
+            read_fields_regex : FieldsArg = None,
+            process_routine : RoutineArgType = None,
         ):
+    if process_routine is not None:
+        logger.debug("routine passed as arg")
     for file in pathlib.Path(dir).glob("*.out"):
-        reader = parse_file(file, read_fields, 
+        reader = parse_file(file,
             read_fields_regex = read_fields_regex, 
             read_fields = read_fields,
             ignore_fields = ignore_fields
