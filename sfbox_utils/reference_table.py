70e0316c2c43703a569f7b22d0a746efca022761
ml <miklakt@gmail.com>
2024-03-07 15:30:31 +0100

    no inplace option, accessor is not nested

--- sfbox_utils/reference_table.py
+++ sfbox_utils/reference_table.py
@@ -27,58 +27,53 @@ def create_reference_dict(
 
 try:
     import pandas as pd
-    def create_reference_table(storage_dir = "h5", load_inplace = False):        
+
+    class H5StorageAccessor:
+        def __init__(self, pandas_obj):
+            self._obj = pandas_obj
+
+        @staticmethod
+        def _validate(obj):
+            if not all(col in obj.columns for col in ['h5file','keys']):
+                raise AttributeError("Columns must include 'h5file', 'keys'")
+            
+        @staticmethod
+        def load_dataset(file, key):
+            file = h5py.File(file)
+            if isinstance(key, list):
+                data = [np.array(file[k]) for k in key]
+            else:
+                data = np.array(file[key])
+            file.close()
+            return data
+            
+        def __getitem__(self, keys):
+            if not isinstance(keys, list):
+                keys = [keys]
+            else:
+                df = pd.DataFrame(columns=keys)
+
+            for key in keys:
+                df[key] = df[key].astype(object) #in case of inhomogenous data
+                df[key] = self._obj.apply(lambda _: H5StorageAccessor.load_dataset(_.h5file, f"/{key}"), axis=1)
+            
+            return df[keys]
         
-        class H5StorageAccessor:
-            def __init__(self, pandas_obj):
-                self._obj = pandas_obj
+    pd.api.extensions.register_dataframe_accessor("dataset")(H5StorageAccessor)
 
-            @staticmethod
-            def _validate(obj):
-                if not all(col in obj.columns for col in ['h5file','keys']):
-                    raise AttributeError("Columns must include 'h5file', 'keys'")
-                
-            @staticmethod
-            def load_dataset(file, key):
-                file = h5py.File(file)
-                if isinstance(key, list):
-                    data = [np.array(file[k]) for k in key]
-                else:
-                    data = np.array(file[key])
-                file.close()
-                return data
-                
-            def __getitem__(self, keys):
-                if not isinstance(keys, list):
-                    keys = [keys]
-                if load_inplace:
-                    df = self._obj
-                else:
-                    df = pd.DataFrame(columns=keys)
+    class H5StorageAccessorSeries(H5StorageAccessor):
+        def __getitem__(self, keys):
+            if not isinstance(keys, list):
+                keys = [keys]
+            else:
+                df = pd.Series(index=keys, dtype = object)
+            for key in keys:
+                loaded_data = H5StorageAccessor.load_dataset(self._obj.h5file, f"/{key}")
+                df[key] = loaded_data
+            return df[keys].squeeze()
+    pd.api.extensions.register_series_accessor("dataset")(H5StorageAccessorSeries)
 
-                for key in keys:
-                    if load_inplace: df[key] = np.nan #create column in the original dataframe
-                    df[key] = df[key].astype(object) #in case of inhomogenous data
-                    df[key] = self._obj.apply(lambda _: H5StorageAccessor.load_dataset(_.h5file, f"/{key}"), axis=1)
-                
-                return df[keys]
-            
-        pd.api.extensions.register_dataframe_accessor("dataset")(H5StorageAccessor)
-
-        class H5StorageAccessorSeries(H5StorageAccessor):
-            def __getitem__(self, keys):
-                if not isinstance(keys, list):
-                    keys = [keys]
-                if load_inplace:
-                    df = self._obj
-                else:
-                    df = pd.Series(index=keys, dtype = object)
-                for key in keys:
-                    loaded_data = H5StorageAccessor.load_dataset(self._obj.h5file, f"/{key}")
-                    df[key] = loaded_data
-                return df[keys].squeeze()
-        pd.api.extensions.register_series_accessor("dataset")(H5StorageAccessorSeries)
-            
+    def create_reference_table(storage_dir):        
         dataframe = pd.DataFrame(create_reference_dict(storage_dir))
         return dataframe
             
